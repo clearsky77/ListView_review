2945c9670a9121d10a2bd91d246a100b32e48b4a
clearsky77 <clearsky3264@gmail.com>
2021-09-08 21:05:59 +0900

    row 결과를 return

--- app/src/main/java/com/clearsky77/listview_review/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_review/adapters/StudentAdapter.kt
@@ -2,7 +2,10 @@ package com.clearsky77.listview_review.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.clearsky77.listview_review.R
 import com.clearsky77.listview_review.datas.StudentData
 
 class StudentAdapter(
@@ -11,6 +14,18 @@ class StudentAdapter(
     val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList) { //<데이터 표현용 클래스>
                                             //StudentAdapter을 생성할 때 받아 온것을 부모에게도 넘겨 준다.
     val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+//        return super.getView(position, convertView, parent) 리턴 슈퍼는 꼭 지우세요
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.student_list_item, null) //Student_list_item.xml 틀을 가져와서
+        }
+//        tempRow는 null이 절대 될 수 없다.
+        val row = tempRow!!
+
+        return row
+    }
 
 
 }
