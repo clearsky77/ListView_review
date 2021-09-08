5cf4ae272c29f8a36968ac53c777eec37f60cc4d
clearsky77 <clearsky3264@gmail.com>
2021-09-08 19:38:57 +0900

    StudentAdapter를 쓰려면 필요한 매개변수 받도록 구현

--- app/src/main/java/com/clearsky77/listview_review/adapters/StuentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_review/adapters/StuentAdapter.kt
@@ -1,6 +1,11 @@
 package com.clearsky77.listview_review.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
+import com.clearsky77.listview_review.datas.StudentData
 
-class StuentAdapter : ArrayAdapter<StuentAdapter> (){
+class StuentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>() {
 }
